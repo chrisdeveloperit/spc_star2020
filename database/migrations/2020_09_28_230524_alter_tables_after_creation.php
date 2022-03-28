c8fe81b09878eb4834d4e87e713bdf3e014548e8
Eric Sheppard <eric@newsystemtechnologies.com>
2022-03-28 13:39:56 -0400

    - remove reference to people table

--- database/migrations/2020_09_28_230524_alter_tables_after_creation.php
+++ database/migrations/2020_09_28_230524_alter_tables_after_creation.php
@@ -13,18 +13,18 @@ class AlterTablesAfterCreation extends Migration
      */
     public function up()
     {
-        $statementPeople = "ALTER TABLE people AUTO_INCREMENT = 1475;";
-        DB::unprepared($statementPeople);
-		
+        // $statementPeople = "ALTER TABLE people AUTO_INCREMENT = 1475;";
+        // DB::unprepared($statementPeople);
+
 		$statementOrg = "ALTER TABLE organizations AUTO_INCREMENT = 2000;";
         DB::unprepared($statementOrg);
-		
+
 		$statementMeters = "ALTER TABLE meters AUTO_INCREMENT = 15000000;"; //current max = 12860936
         DB::unprepared($statementMeters);
-    
+
     	/*$statementMachineStatus = "ALTER TABLE y2019_machine_status ADD INDEX idx_serNum_date (serial_number, date_timestamp)";
    		DB::unprepared($statementMachineStatus);*/
-   		
+
    		//DB::statement('ALTER TABLE buildings ENGINE = InnoDB'); just a thought
     }
 
